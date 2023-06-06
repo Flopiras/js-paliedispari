77169b2b3e001ce628103e34bde4ff08530f5205
Flopiras <Flopiras@yahoo.it>
2023-06-06 18:30:58 +0200

    fix ex.1

--- js/script-ex1.js
+++ js/script-ex1.js
@@ -12,49 +12,38 @@ button.addEventListener('click', function(){
     const userWord = wordInput.value.trim();
      console.log(userWord);
 
-     // Scomporre la parola in singole lettere
-    let userLetters = [];
-
-    //creare un ciclo che trasformi le lettere in stringhe
-    for(let i = 0; i < userWord.length ; i++){
-        //console.log(userWord.charAt(i));
-
-        //pushare le stringhe in un'array
-        userLetters.push(userWord.charAt(i));
-    }
- console.log(userLetters);
-
     //Ricomporre la parola partendo dall'ultima lettera fino alla prima
-    const turnWord = [];
+    let turnWord = '';
     
     for(let i = userWord.length - 1; i >= 0; i--){
         //console.log(userWord.charAt(i));
 
-        turnWord.push(userWord.charAt(i));
+        turnWord +=(userWord.charAt(i));
     }
   
  console.log(turnWord)
-
-    let result = ' ';
+ console.log(userWord);
 
     //Comparare la parola di partenza con quella ottenuta
-    function compare(turnWord, userLetters){
+    let result = compare(turnWord, userWord);
+    console.log(result)
 
-        //SE le parole sono uguali, stampare in pagina 'SI'
-        if(turnWord == userLetters){
-            
-            result = 'SI'
-        }
-        //ALTRIMENTI stampare in pagina 'NO'
-        else{
-            result= 'NO'
-
-        }
-    }
-    
-    compare();
-    
-    //3. stampo in pagina
+    //4. stampo in pagina
     answer.innerText = result;
 })
 
+ //Creare una funzione di comparazione
+ function compare(a, b){
+    let isEqual;
+    //SE le parole sono uguali, stampare in pagina 'SI'
+    if(a == b){
+        
+        isEqual = 'SI'
+    }
+    //ALTRIMENTI stampare in pagina 'NO'
+    else{
+        isEqual= 'NO'
+
+    }
+    return isEqual;
+}
--- readme.md
+++ readme.md
@@ -13,7 +13,20 @@ Creare una funzione per capire se la parola inserita è palindroma
      - creare un ciclo che trasformi le lettere in stringhe
      - pushare le stringhe in un'array
    - Ricomporre la parola partendo dall'ultima lettera fino alla prima
+3. Creare una funzione di comparazione
    - Comparare la parola di partenza con quella ottenuta
      - **SE** le parole sono uguali, stampare in pagina 'SI'
      - **ALTRIMENTI** stampare in pagina 'NO'
-3. stampo in pagina
+4. stampo in pagina
+
+<hr>
+
+**PARI E DISPARI**
+
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+**TO DO**
