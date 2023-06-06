e884bbc7c5fb44f9a05a35097c0d178d0c2ffc7b
Flopiras <Flopiras@yahoo.it>
2023-06-06 17:47:15 +0200

    ex.1

--- js/script-ex1.js
+++ js/script-ex1.js
@@ -1,1 +1,60 @@
 console.log('js ok')
+
+//1. Prendere l'elemento dal DOM
+const wordInput = document.getElementById('word');
+const button = document.getElementById('palindrome-button');
+const answer = document.getElementById('palindrome-answer');
+
+//2. Mettere in ascolto il bottone
+button.addEventListener('click', function(){
+    
+    // Individuare la parola inserita dall'utente
+    const userWord = wordInput.value.trim();
+     console.log(userWord);
+
+     // Scomporre la parola in singole lettere
+    let userLetters = [];
+
+    //creare un ciclo che trasformi le lettere in stringhe
+    for(let i = 0; i < userWord.length ; i++){
+        //console.log(userWord.charAt(i));
+
+        //pushare le stringhe in un'array
+        userLetters.push(userWord.charAt(i));
+    }
+ console.log(userLetters);
+
+    //Ricomporre la parola partendo dall'ultima lettera fino alla prima
+    const turnWord = [];
+    
+    for(let i = userWord.length - 1; i >= 0; i--){
+        //console.log(userWord.charAt(i));
+
+        turnWord.push(userWord.charAt(i));
+    }
+  
+ console.log(turnWord)
+
+    let result = ' ';
+
+    //Comparare la parola di partenza con quella ottenuta
+    function compare(turnWord, userLetters){
+
+        //SE le parole sono uguali, stampare in pagina 'SI'
+        if(turnWord == userLetters){
+            
+            result = 'SI'
+        }
+        //ALTRIMENTI stampare in pagina 'NO'
+        else{
+            result= 'NO'
+
+        }
+    }
+    
+    compare();
+    
+    //3. stampo in pagina
+    answer.innerText = result;
+})
+
--- readme.md
+++ readme.md
@@ -6,11 +6,14 @@ Creare una funzione per capire se la parola inserita è palindroma
 **TO DO**
 
 1. Prendere l'elemento dal DOM
-2. Individuare la parola inserita dall'utente
-3. Scomporre la parola in singole lettere
-   - trasformare le lettere in stringhe
-   - pushare le stringhe in un'array
-4. Ricomporre la parola partendo dall'ultima lettera fino alla prima
-5. Comparare la parola di partenza con quella ottenuta
-   - **SE** le parole sono uguali, stampare in pagina 'SI'
-   - **ALTRIMENTI** stampare in pagina 'NO'
+2. Mettere in ascolto il bottone
+   - Al suo click
+   - Individuare la parola inserita dall'utente
+     - Scomporre la parola in singole lettere
+     - creare un ciclo che trasformi le lettere in stringhe
+     - pushare le stringhe in un'array
+   - Ricomporre la parola partendo dall'ultima lettera fino alla prima
+   - Comparare la parola di partenza con quella ottenuta
+     - **SE** le parole sono uguali, stampare in pagina 'SI'
+     - **ALTRIMENTI** stampare in pagina 'NO'
+3. stampo in pagina
